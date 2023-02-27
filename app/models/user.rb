7ea30f29ea2e021688f634bcbcf2147a45b29579
Julien Discrit <juliendiscrit@gmail.com>
2023-02-27 18:26:06 +0100

    added validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  has_many :costumes
+  has_many :bookings
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :description, length: { minimum: 6 }
+  validates :address, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
