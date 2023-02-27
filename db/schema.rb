87118ecb3a75f0289edccf51270815aeeb1f5d0e
Julien Martin <julien.martin94@icloud.com>
2023-02-27 17:41:53 +0100

    column removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_150840) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_163927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_150840) do
     t.text "description"
     t.boolean "available"
     t.integer "costume_rating"
-    t.text "costume_review"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
