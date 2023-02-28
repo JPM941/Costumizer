a9a2d5bed98ab480ce708a8f1a233762f5ae124b
Dulcie IRVANI <dulcie.irvani@gmail.com>
2023-02-28 16:10:20 +0100

    fixed seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,9 @@ require 'open-uri'
 
 I18n.reload!
 
-User.destroy_all
-Costume.destroy_all
 Booking.destroy_all
-
+Costume.destroy_all
+User.destroy_all
 
   julien = User.create!(
     email: "Julien@lewagon.fr",
