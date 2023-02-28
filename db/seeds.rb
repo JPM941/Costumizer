6279c5e00ce65b54d4429e90ddf76725737f8a56
Mathieu Zimeris <alexvargas@hotmail.fr>
2023-02-28 12:00:48 +0100

    amélioration de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,15 @@ I18n.reload!
 Costume.destroy_all
 User.destroy_all
 
-5.times do
+
   user = User.create!(
-    email: Faker::Internet.email,
+    email: "Julien@lewagon.fr",
     password: "password",
     # encrypted_password: '#$taawktljasktlw4aaglj',
-    user_name: Faker::Internet.username,
-    description: Faker::Lorem.paragraph,
+    user_name: "Julien",
+    description: "Le grand patron",
     address: Faker::Address.city,
   )
-  p user
   (0..5).to_a.sample.times do
     costume = Costume.create!(
       name: Faker::Commerce.product_name,
@@ -32,23 +31,66 @@ User.destroy_all
     )
   p costume
   end
-end
+
+  user = User.create!(
+    email: "Dulcie@lewagon.fr",
+    password: "password",
+    # encrypted_password: '#$taawktljasktlw4aaglj',
+    user_name: "Dulcie",
+    description: "Le front c'est trop bien !",
+    address: Faker::Address.city,
+  )
+  (0..5).to_a.sample.times do
+    costume = Costume.create!(
+      name: Faker::Commerce.product_name,
+      price: (10..500).to_a.sample,
+      description: Faker::Lorem.paragraph,
+      costume_rating: (0..5).to_a.sample,
+      user_id: user[:id]
+    )
+  p costume
+  end
+
+  user = User.create!(
+    email: "Mathieu@lewagon.fr",
+    password: "password",
+    # encrypted_password: '#$taawktljasktlw4aaglj',
+    user_name: "Mathieu",
+    description: "Le back c'est trop bien !",
+    address: Faker::Address.city,
+  )
+  (0..5).to_a.sample.times do
+    costume = Costume.create!(
+      name: Faker::Commerce.product_name,
+      price: (10..500).to_a.sample,
+      description: Faker::Lorem.paragraph,
+      costume_rating: (0..5).to_a.sample,
+      user_id: user[:id]
+    )
+  p costume
+  end
+
+
+
+
+
+
 p "seed done"
 
 
-# require "open-uri"
-# require "nokogiri"
-# require "json"
+require "open-uri"
+require "nokogiri"
+require "json"
 
-# @n ||= 0
+@n ||= 0
 
-# url = "https://www.google.com/search?q=vetements+bizarres&source=lnms&tbm=isch&sa=X&ved=2ahUKEwilk5KMzbb9AhWeTKQEHeVhDPUQ_AUoAXoECAEQAw"
+url = "https://www.google.com/search?q=vetements+bizarres&source=lnms&tbm=isch&sa=X&ved=2ahUKEwilk5KMzbb9AhWeTKQEHeVhDPUQ_AUoAXoECAEQAw"
 
-# html_file = URI.open(url)
-# html_doc = Nokogiri::HTML.parse(html_file)
-# puts "                                            search                                            "
-# html_doc.search("#vplap#{@n}.plantl pla-unit-single-clickable-target clickable-card").each do |element|
-#   puts @n
-#   puts element[0]
-#   @n += 1
-# end
+html_file = URI.open(url)
+html_doc = Nokogiri::HTML.parse(html_file)
+puts "                                            search                                            "
+html_doc.search("vplap#{@n}").each do |element|
+  puts @n
+  puts element[0]
+  @n += 1
+end
