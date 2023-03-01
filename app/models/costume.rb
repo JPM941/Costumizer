bb4854d652c95c3472ae4ca9a46d40d66b6576a4
Julien Martin <julien.martin94@icloud.com>
2023-03-01 16:48:22 +0100

    geocoding

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,6 +3,9 @@ class Costume < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many_attached :images
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  
   validates :name, presence: true, length: { minimum: 2 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { minimum: 6 }
