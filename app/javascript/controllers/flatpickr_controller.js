43b2158e9c97fac000bdf4b0f55d6342b7617a71
Julien Discrit <juliendiscrit@gmail.com>
2023-03-01 17:14:09 +0100

    flat pickr new booking calendrier

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,14 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr";
+import flatpickr from "flatpickr"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-// Connects to data-controller="flatpickr"
 export default class extends Controller {
+
+  static targets = [ 'startDateInput', 'endDateInput' ]
   connect() {
-    new flatpickr(this.element, {
-      enableTime: true
-      // more options available on the documentation!
-    });
+    flatpickr(this.startDateInputTarget, {
+      inline: true,
+      mindate: "today",
+      mode: 'range',
+      "plugins": [new rangePlugin({ input: this.endDateInputTarget})]
+    })
   }
 }
-
-
